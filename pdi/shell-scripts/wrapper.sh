53a9684fbcca91448558ce354d01519a31b2374e
Diethard Steiner <diethard.steiner@gmail.com>
2020-01-09 16:57:48 +0000

    adjusted function to determine path to sourced wrapper script to work with zsh

--- pdi/shell-scripts/wrapper.sh
+++ pdi/shell-scripts/wrapper.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
 # ======================= INPUT ARGUMENTS ============================ #
 
@@ -53,19 +53,21 @@ fi
 # we have to get the path to this shell script from any location we execute this script from
 # and with both ./ and source
 # https://unix.stackexchange.com/questions/4650/determining-path-to-sourced-shell-script
-if [[ $0 != ${BASH_SOURCE} ]] 
-  then
-    echo "Script is being sourced"
-    # this didn't seem to work consistently across all versions of bash
-    # for both source and script approaches hence we use this if condition here
-    # WRAPPER_DIR=${BASH_SOURCE[${#BASH_SOURCE[@]} - 1]}
-    WRAPPER_DIR=${BASH_SOURCE%/*}
-  else
-    # There shouldn't really be a use case for running this script directly
-    echo "Script is being run"
-    WRAPPER_DIR="$( cd "$( /usr/bin/dirname "$0" )" && pwd )"
-fi
-
+# if [[ $0 != ${BASH_SOURCE} ]] 
+#   then
+#     echo "Script is being sourced"
+#     # this didn't seem to work consistently across all versions of bash
+#     # for both source and script approaches hence we use this if condition here
+#     # WRAPPER_DIR=${BASH_SOURCE[${#BASH_SOURCE[@]} - 1]}
+#     WRAPPER_DIR=${BASH_SOURCE%/*}
+#   else
+#     # There shouldn't really be a use case for running this script directly
+#     echo "Script is being run"
+#     WRAPPER_DIR="$( cd "$( /usr/bin/dirname "$0" )" && pwd )"
+# fi
+# this didn't work reliably - resorted to this:
+# source: https://unix.stackexchange.com/questions/76505/portable-way-to-get-scripts-absolute-path
+WRAPPER_DIR=${0:a:h}
 
 # WRAPPER_DIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 echo "WRAPPER_DIR: ${WRAPPER_DIR}"
@@ -122,7 +124,7 @@ PROJECT_LOG_HOME="${PROJECT_GROUP_DIR}/logs/${PDI_ENV}"
 # since code for different projects might run on the same server
 
 # path to PDI installation
-PDI_DIR=/home/dsteiner/apps/pdi-ce-8.1
+PDI_DIR=/home/dsteiner/apps/kettle-neo4j-remix-beam-8.2.0.7-719-REMIX
 echo "PDI_DIR: ${PDI_DIR}"
 export KETTLE_HOME=${PROJECT_CONFIG_DIR}/pdi
 echo "KETTLE_HOME: ${KETTLE_HOME}"
@@ -145,108 +147,127 @@ echo "UNIT_TESTS_BASE_PATH: ${UNIT_TESTS_BASE_PATH}"
 # if we just want to set the env variables for Spoon, then do not run this part
 if [ ! -z "$1" ] && [ ! -z "$2" ]; then
 
-# Project logs file name
-JOB_LOG_FILE="${JOB_NAME}.err.log"
-# Project historic logs filename
-JOB_LOG_HIST_FILE="${JOB_NAME}.hist.log"
+  # Project logs file name
+  JOB_LOG_FILE="${JOB_NAME}.err.log"
+  # Project historic logs filename
+  JOB_LOG_HIST_FILE="${JOB_NAME}.hist.log"
+  
+  
+  START_DATETIME=`date '+%Y-%m-%d_%H-%M-%S'`
+  START_UNIX_TIMESTAMP=`date "+%s"`
+  
+  
+  mkdir -p ${PROJECT_LOG_HOME}
+  
+  
+  echo "Location of log file: ${PROJECT_LOG_HOME}/${JOB_LOG_FILE}"
+  
+  # remove old log file
+  if [ -f "${PROJECT_LOG_HOME}/${JOB_LOG_FILE}" ]
+  then
+      echo "Removing old log file."
+      rm ${PROJECT_LOG_HOME}/${JOB_LOG_FILE}
+  else
+      echo "No old log file exists ... so nothing to do."
+  fi
+  
+  
+  # moved this further down since otherwise the kitchen logFile flag does not work
+  # cat > ${PROJECT_LOG_HOME}/${JOB_LOG_FILE} <<EOL
+  
+  # Starting at: ${START_DATETIME}
+  
+  # -----------------------------------------------------------------------
+  # Running script with the following environment variables:
+  
+  # PDI Environment (PDI_ENV):                   ${PDI_ENV}
+  # Directory containing PDI installation:       ${PDI_DIR}
+  # Location of Kettle properties (KETTLE_HOME): ${KETTLE_HOME}
+  # Location of Kettle Metastore:                ${PENTAHO_METASTORE_FOLDER}
+  # Location of Project Configuration:           ${PROJECT_CONFIG_DIR}
+  # Location of Project Code:                    ${PROJECT_CODE_DIR}
+  # PDI Job Directory and Filename:              ${JOB_HOME}${JOB_NAME}
+  # Location of log file:                        ${PROJECT_LOG_HOME}/${JOB_LOG_FILE}
+  # -----------------------------------------------------------------------
+  
+  # EOL
+  
+  
+  
+  # ====================== PDI KITCHEN WRAPPER ========================= #
+  
+  ## ~~~~~~~~~~~~~~~~~~~~~~~~ DO NOT CHANGE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+  ##                      ______________________                        ##
+  
+  
+  cd ${PDI_DIR}
+  
+  # Note: the parameters passed here to the kitchen command have to be defined
+  # as parameters in the Kettle/PDI job that gets called otherwise they won't
+  # be passed on.
+  
+  ./kitchen.sh \
+  -file="${PROJECT_CODE_PDI_DIR}/${JOB_HOME}/${JOB_NAME}" \
+  -logfile=${PROJECT_LOG_HOME}/${JOB_LOG_FILE} \
+  -param:PROJECT_CONFIG_DIR=${PROJECT_CONFIG_DIR} \
+  -param:PROJECT_CODE_DIR=${PROJECT_CODE_DIR} \
+  -param:JOB_LOG_FILE=${JOB_LOG_FILE} \
+  -param:PDI_ENV=${PDI_ENV} \
+  -param:PROJECT_NAME=${PROJECT_NAME} \
+  2>&1
+  
+  
+  RES=$?
+  
+    
+  END_DATETIME=`date '+%Y-%m-%d_%H-%M-%S'`
+  END_UNIX_TIMESTAMP=`date "+%s"`
+  echo
+  echo "End DateTime: ${END_DATETIME}" >> ${PROJECT_LOG_HOME}/${JOB_LOG_FILE}
 
-
-START_DATETIME=`date '+%Y-%m-%d_%H-%M-%S'`
-START_UNIX_TIMESTAMP=`date "+%s"`
-
-
-mkdir -p ${PROJECT_LOG_HOME}
-
-
-echo "Location of log file: ${PROJECT_LOG_HOME}/${JOB_LOG_FILE}"
-
-# remove old log file
-if [ -f "${PROJECT_LOG_HOME}/${JOB_LOG_FILE}" ]
-then
-    echo "Removing old log file."
-    rm ${PROJECT_LOG_HOME}/${JOB_LOG_FILE}
-else
-    echo "No old log file exists ... so nothing to do."
-fi
-
-
-# moved this further down since otherwise the kitchen logFile flag does not work
-# cat > ${PROJECT_LOG_HOME}/${JOB_LOG_FILE} <<EOL
-
-# Starting at: ${START_DATETIME}
-
-# -----------------------------------------------------------------------
-# Running script with the following environment variables:
-
-# PDI Environment (PDI_ENV):                   ${PDI_ENV}
-# Directory containing PDI installation:       ${PDI_DIR}
-# Location of Kettle properties (KETTLE_HOME): ${KETTLE_HOME}
-# Location of Kettle Metastore:                ${PENTAHO_METASTORE_FOLDER}
-# Location of Project Configuration:           ${PROJECT_CONFIG_DIR}
-# Location of Project Code:                    ${PROJECT_CODE_DIR}
-# PDI Job Directory and Filename:              ${JOB_HOME}${JOB_NAME}
-# Location of log file:                        ${PROJECT_LOG_HOME}/${JOB_LOG_FILE}
-# -----------------------------------------------------------------------
-
-# EOL
-
-
-
-# ====================== PDI KITCHEN WRAPPER ========================= #
-
-## ~~~~~~~~~~~~~~~~~~~~~~~~ DO NOT CHANGE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
-##                      ______________________                        ##
-
-
-cd ${PDI_DIR}
-
-# Note: the parameters passed here to the kitchen command have to be defined
-# as parameters in the Kettle/PDI job that gets called otherwise they won't
-# be passed on.
-
-./kitchen.sh \
--file="${PROJECT_CODE_PDI_DIR}/${JOB_HOME}/${JOB_NAME}" \
--logfile=${PROJECT_LOG_HOME}/${JOB_LOG_FILE} \
--param:PROJECT_CONFIG_DIR=${PROJECT_CONFIG_DIR} \
--param:PROJECT_CODE_DIR=${PROJECT_CODE_DIR} \
--param:JOB_LOG_FILE=${JOB_LOG_FILE} \
--param:PDI_ENV=${PDI_ENV} \
--param:PROJECT_NAME=${PROJECT_NAME} \
-2>&1
-
-
-RES=$?
-
-END_DATETIME=`date '+%Y-%m-%d_%H-%M-%S'`
-END_UNIX_TIMESTAMP=`date "+%s"`
-echo
-echo "End DateTime: ${END_DATETIME}" >> ${PROJECT_LOG_HOME}/${JOB_LOG_FILE}
-
+# the EOL section can't be indented - otherwise it will throw error 
 cat >> ${PROJECT_LOG_HOME}/${JOB_LOG_FILE} <<EOL
 
-END_DATETIME=`date '+%Y-%m-%d_%H-%M-%S'`
-END_UNIX_TIMESTAMP=`date "+%s"`
-echo
-echo "End DateTime: ${END_DATETIME}" >> ${PROJECT_LOG_HOME}/${JOB_LOG_FILE}
+Starting at: ${START_DATETIME}
 
+-----------------------------------------------------------------------
+Running script with the following environment variables:
 
-DURATION_IN_SECONDS=`expr ${END_UNIX_TIMESTAMP} - ${START_UNIX_TIMESTAMP}`
-#DURATION_IN_MINUTES=`echo "scale=0;${DURATION_IN_SECONDS}/60" | bc`
-DURATION_IN_SECONDS_MSG=`printf '%dh:%dm:%ds\n' $((${DURATION_IN_SECONDS}/(60*60))) $((${DURATION_IN_SECONDS}%(60*60)/60)) $((${DURATION_IN_SECONDS}%60))`
+PDI Environment (PDI_ENV):                   ${PDI_ENV}
+Directory containing PDI installation:       ${PDI_DIR}
+Location of Kettle properties (KETTLE_HOME): ${KETTLE_HOME}
+Location of Kettle Metastore:                ${PENTAHO_METASTORE_FOLDER}
+Location of Project Configuration:           ${PROJECT_CONFIG_DIR}
+Location of Project Code:                    ${PROJECT_CODE_DIR}
+PDI Job Directory and Filename:              ${JOB_HOME}${JOB_NAME}
+Location of log file:                        ${PROJECT_LOG_HOME}/${JOB_LOG_FILE}
+-----------------------------------------------------------------------
 
-# Project historic logs filename
-JOB_LOG_HIST_FILE="${JOB_NAME}.hist.log"
-# Project archive logs filename
-PROJECT_LOG_ARCHIVE_FILE="${JOB_NAME}_${END_DATETIME}.err.log"
+EOL
 
-# Get the duration in human-readable format
-# DURATION=`grep "Processing ended after " ${PROJECT_LOG_HOME}/${JOB_LOG_FILE} | sed -n -e 's/^.*Processing ended after after //p'`
-
-echo "Result: ${RES}"
-# DURATION_IN_SECONDS calc missing
-echo "Start: ${START_DATETIME} END: ${END_DATETIME} Result: ${RES} Duration: ${DURATION_IN_SECONDS_MSG} - Duration in Seconds: ${DURATION_IN_SECONDS}s" >> ${PROJECT_LOG_HOME}/${JOB_LOG_HIST_FILE}
-cat ${PROJECT_LOG_HOME}/${JOB_LOG_FILE} > ${PROJECT_LOG_HOME}/${PROJECT_LOG_ARCHIVE_FILE}
-
-exit ${RES}
-
+  
+  END_DATETIME=`date '+%Y-%m-%d_%H-%M-%S'`
+  END_UNIX_TIMESTAMP=`date "+%s"`
+  echo
+  echo "End DateTime: ${END_DATETIME}" >> ${PROJECT_LOG_HOME}/${JOB_LOG_FILE}
+  
+  
+  DURATION_IN_SECONDS=`expr ${END_UNIX_TIMESTAMP} - ${START_UNIX_TIMESTAMP}`
+  #DURATION_IN_MINUTES=`echo "scale=0;${DURATION_IN_SECONDS}/60" | bc`
+  DURATION_IN_SECONDS_MSG=`printf '%dh:%dm:%ds\n' $((${DURATION_IN_SECONDS}/(60*60))) $((${DURATION_IN_SECONDS}%(60*60)/60)) $((${DURATION_IN_SECONDS}%60))`
+  
+  # Project historic logs filename
+  JOB_LOG_HIST_FILE="${JOB_NAME}.hist.log"
+  # Project archive logs filename
+  PROJECT_LOG_ARCHIVE_FILE="${JOB_NAME}_${END_DATETIME}.err.log"
+  
+  # Get the duration in human-readable format
+  # DURATION=`grep "Processing ended after " ${PROJECT_LOG_HOME}/${JOB_LOG_FILE} | sed -n -e 's/^.*Processing ended after after //p'`
+  
+  echo "Result: ${RES}"
+  # DURATION_IN_SECONDS calc missing
+  echo "Start: ${START_DATETIME} END: ${END_DATETIME} Result: ${RES} Duration: ${DURATION_IN_SECONDS_MSG} - Duration in Seconds: ${DURATION_IN_SECONDS}s" >> ${PROJECT_LOG_HOME}/${JOB_LOG_HIST_FILE}
+  cat ${PROJECT_LOG_HOME}/${JOB_LOG_FILE} > ${PROJECT_LOG_HOME}/${PROJECT_LOG_ARCHIVE_FILE}
+  
+  exit ${RES}
 fi
